7b1c28773739e20814606a4794a13f1d26a7f81f
Alien <67056926+Aliene228@users.noreply.github.com>
2021-02-04 13:50:03 +0300

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "trouble" (
 	"id" serial  NOT NULL,
-	"geo" double precision NOT NULL,
+	"geo" float NOT NULL,
 	"dat" date NOT NULL,
 	"info" varchar(100) NOT NULL,
 	"contact_id" int NOT NULL,
